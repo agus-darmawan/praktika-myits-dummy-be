08e8c74862885d787871a141dc447686be0e4465
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-10-03 23:31:19 +0700

    feat: add model and migration for course

---
+++ database/migrations/1727972759206_create_courses_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'courses'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name', 50).notNullable()
+      table.string('code', 8).notNullable().unique()
+      table.integer('credits').notNullable()
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
